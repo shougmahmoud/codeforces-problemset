ea0937de1ab30b8962c74760e10921a65eeab053
Shoug Mahmoud <71728242+shougmahmoud@users.noreply.github.com>
2022-07-20 01:53:57 +0300

    Add files via upload

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char** argv) {
+	int w;
+	cin>>w;
+	if(w % 2 == 1 || w == 2){
+	cout<<"NO"<<endl;
+	}
+	
+	else{
+	cout<<"YES"<<endl;
+	}
+	
+	return 0;
+}
